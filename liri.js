3a8829d87bcde6e4a292d7c1133943700f1a680f
Michael A <maalbaneze@aol.com>
2019-04-02 07:41:46 -0700

    postTrae

--- liri.js
+++ liri.js
@@ -59,147 +59,148 @@ var getConcert = function (artist) {
         console.log("Date: " + EventData[i].datetime);
         console.log("-----------------------------------");
     };
-
-
-
-    //Function for OMDB Movie info - command is "movie-this"
-    function getMovie() {
-
-        var movieName = userInput;
-        // Then run a request to the OMDB API with the movie specified and use the trilogy API key
-        var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&tomatoes=true&apikey=trilogy";
-
-        request(queryUrl, function (error, response, body) {
-
-            // If the request is successful = 200
-            if (!error && response.statusCode === 200) {
-                var body = JSON.parse(body);
-
-                //Output to console.log ands log.txt
-                console.log('================ Movie Info ================');
-                console.log("Title: " + body.Title);
-                console.log("Release Year: " + body.Year);
-                console.log("IMdB Rating: " + body.imdbRating);
-                console.log("Rotten Tomatoes Rating: " + body.Ratings[2].Value);
-                console.log("Country: " + body.Country);
-                console.log("Language: " + body.Language);
-                console.log("Plot: " + body.Plot);
-                console.log("Actors: " + body.Actors);
-                console.log('==================THE END=================');
-
-            } else {
-                //else - throw as an error
-                console.log("An error has occurred.")
-            }
-            //Response if the user does not type in a movie title
-            if (movieName === "Mr. Nobody") {
-                console.log("-----------------------");
-                console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
-                console.log("It's on Netflix!");
-            }
-        });
-    }
-
-
-    // Create a variable for referencing the Spotify keys in the keys.js file that points to the IDs in the .env file
-    var spotify = new Spotify(keys.spotify);
-
-    // Writes to the log.txt file
-    var getArtistNames = function (artist) {
-        return artist.name;
-    };
-
-    // Function for running a Spotify search - Command is spotify-this-song
-    var getSpotify = function (songName) {
-        if (songName === undefined) {
-            songName = "The Sign";
-            artist = "Ace of Base";
-        }
-
-        spotify.search(
-            {
-                type: "track",
-                query: userAction
-            },
-            function (err, data) {
-                if (err) {
-                    console.log("The following error occurred: " + err);
-                    return;
-                }
-
-                var songs = data.tracks.items();
-
-                for (var i = 0; i < songs.length; i++) {
-                    console.log(i);
-                    logOutput("artist(s): " + songs[i].artists.map(getArtistNames));
-                    logOutput("song name: " + songs[i].name);
-                    logOutput("preview song: " + songs[i].preview_url);
-                    logOutput("album: " + songs[i].album.name);
-                    logOutput("-----------------------------------");
-                }
-            }
-        );
-    };
-
-
-    //Function for command do-what-it-says; reads and splits random.txt file
-    //command is- "do-what-it-says"
-    function doWhatItSays() {
-        //Read random.txt file
-        fs.readFile("random.txt", "utf8", function (error, data) {
-            if (!error);
-            console.log(data.toString());
-            //split text with comma delimiter
-            var cmds = data.toString().split(',');
-        });
-    }
-
-    //Options function for the four commands 
-    function queryOptions(userAction) {
-
-        //choose which statement (userAction) to switch to and execute
-        switch (userAction) {
-
-            case "concert-this":
-                getConcert();
-                break;
-
-            case "spotify-this-song":
-                getSpotify();
-                break;
-
-            case "movie-this":
-                getMovie();
-                break;
-
-            case "do-what-it-says":
-                doWhatItSays();
-                break;
-        }
-
-
-        // fs is a core Node package for reading and writing files
-        var fs = require("fs");
-
-        // The code will store the contents of reading random.txt inside the variable "data"
-        fs.readFile("random.txt", "utf8", function (error, data) {
-
-            // If the code experiences any errors it will log the error to the console.
-            if (error) {
-                return console.log(error);
-            }
-
-            // We will then print the contents of data
-            console.log(data);
-
-            // Then split it by commas (to make it more readable)
-            var dataArr = data.split(", ");
-
-            // We will then re-display the content as an array for later use.
-            console.log(dataArr);
-
-        })
-    };
-    //Call queryOptions function
-    queryOptions(userAction);
 }
+
+//Function for OMDB Movie info - command is "movie-this"
+function getMovie() {
+
+    var movieName = userInput;
+    // Then run a request to the OMDB API with the movie specified and use the trilogy API key
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&tomatoes=true&apikey=trilogy";
+
+    request(queryUrl, function (error, response, body) {
+
+        // If the request is successful = 200
+        if (!error && response.statusCode === 200) {
+            var body = JSON.parse(body);
+
+            //Output to console.log ands log.txt
+            console.log('================ Movie Info ================');
+            console.log("Title: " + body.Title);
+            console.log("Release Year: " + body.Year);
+            console.log("IMdB Rating: " + body.imdbRating);
+            console.log("Rotten Tomatoes Rating: " + body.Ratings[2].Value);
+            console.log("Country: " + body.Country);
+            console.log("Language: " + body.Language);
+            console.log("Plot: " + body.Plot);
+            console.log("Actors: " + body.Actors);
+            console.log('==================THE END=================');
+
+        } else {
+            //else - throw as an error
+            console.log("An error has occurred.")
+        }
+        //Response if the user does not type in a movie title
+        if (movieName === "Mr. Nobody") {
+            console.log("-----------------------");
+            console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
+            console.log("It's on Netflix!");
+        }
+    });
+}
+
+
+// Create a variable for referencing the Spotify keys in the keys.js file that points to the IDs in the .env file
+var spotify = new Spotify(keys.spotify);
+
+// Writes to the log.txt file
+var getArtistNames = function (artist) {
+    return artist.name;
+};
+
+// Function for running a Spotify search - Command is spotify-this-song
+var getSpotify = function (songName) {
+    if (songName === undefined) {
+        songName = "The Sign";
+        artist = "Ace of Base";
+    }
+
+    spotify.search(
+        {
+            type: "track",
+            query: userAction
+        },
+        function (err, data) {
+            if (err) {
+                console.log("The following error occurred: " + err);
+                return;
+            }
+
+            var songs = data.tracks.items;
+
+            for (var i = 0; i < songs.length; i++) {
+                console.log(i);
+                logOutput("artist(s): " + songs[i].artists.map(getArtistNames));
+                logOutput("song name: " + songs[i].name);
+                logOutput("preview song: " + songs[i].preview_url);
+                logOutput("album: " + songs[i].album.name);
+                logOutput("-----------------------------------");
+            }
+        }
+    );
+};
+
+
+//Function for command do-what-it-says; reads and splits random.txt file
+//command is- "do-what-it-says"
+function doWhatItSays() {
+    //Read random.txt file
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (!error);
+        console.log(data.toString());
+        //split text with comma delimiter
+        var cmds = data.toString().split(',');
+    });
+}
+
+//Options function for the four commands 
+function queryOptions(userAction) {
+
+    //choose which statement (userAction) to switch to and execute
+    switch (userAction) {
+
+        case "concert-this":
+            getConcert();
+            break;
+
+        case "spotify-this-song":
+            getSpotify();
+            break;
+
+        case "movie-this":
+            getMovie();
+            break;
+
+        case "do-what-it-says":
+            doWhatItSays();
+            break;
+    }
+
+
+    // fs is a core Node package for reading and writing files
+    var fs = require("fs");
+
+    // The code will store the contents of reading random.txt inside the variable "data"
+    fs.readFile("random.txt", "utf8", function (error, data) {
+
+        // If the code experiences any errors it will log the error to the console.
+        if (error) {
+            return console.log(error);
+        }
+
+        // We will then print the contents of data
+        console.log(data);
+
+        // Then split it by commas (to make it more readable)
+        var dataArr = data.split(", ");
+
+        // We will then re-display the content as an array for later use.
+        console.log(dataArr);
+
+    })
+};
+
+//Call queryOptions function
+
+queryOptions(process.argv[2]);
+console.log(process.argv[2]);
